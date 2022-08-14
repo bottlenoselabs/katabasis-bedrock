870d46f80c24fd32aa49b605b8a4059814246772
Lucas Girouard-Stranks <lustranks@gmail.com>
2022-08-14 02:07:33 -0400

    Fix build.sh

--- build.sh
+++ build.sh
@@ -7,7 +7,4 @@ fi
 
 VERSION="$(date +%Y.%-m).$(git rev-list --count $1)"
 dotnet build --nologo --verbosity minimal --configuration Release -p:Version=$VERSION
-if [[ "$2" == "ubuntu-18.04" ]]; then
-    dotnet pack "./src/cs/production/Katabasis.Bedrock/Katabasis.Bedrock.csproj" --nologo --verbosity minimal --configuration Release --no-build -p:PackageVersion="$VERSION"
-fi
 dotnet pack "./src/cs/production/Katabasis.Bedrock.$3/Katabasis.Bedrock.$3.csproj" --nologo --verbosity minimal --configuration Release --no-build -p:PackageVersion="$VERSION"
